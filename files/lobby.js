24a1a81615b866e7ec93cc96741aaaf1022eae65
Gabriel Solotorevsky <gabysolotorevsky100@gmail.com>
2024-02-12 20:14:01 -0300

    Errores

--- files/lobby.js
+++ files/lobby.js
@@ -7,11 +7,14 @@ let Chat = document.getElementById('chat');
 Chat.hidden = true;
 let Salir = document.getElementById('salir');
 let Nombre = document.getElementById('nombreChat');
+let Msg = document.getElementById('mensajes');
+let Mensajeador = document.getElementById('meth');
 
 if (localStorage['destino']){
     Consola.hidden = true;
     Chat.hidden = false;
     Nombre.innerHTML = "Lobby " + localStorage['destino'];
+    ConectarseServer();
 }
 
 Volver.addEventListener('click', function(){
@@ -32,3 +35,51 @@ ConsolaForm.addEventListener('submit', function(a){
     localStorage['destino'] = Destino.value;
     Nombre.innerHTML = "Lobby " + localStorage['destino'];
 })
+
+function CrearMensaje(usuario,destino,mensaje){
+    if (localStorage['destino'] == destino){
+        let MensajeElemento = document.createTextNode(usuario + ":" + mensaje);
+        Msg.appendChild(MensajeElemento);
+    }
+}
+
+function ConectarseServer(){
+    if (localStorage['destino']) {
+        const ServerEvents = new EventSource("/server");
+        ServerEvents.addEventListener('message', (s) => {
+            console.log("raw data", JSON.stringify(s.data));
+          console.log("decoded data", JSON.stringify(JSON.parse(s.data)));
+          let ms = JSON.parse(s.data);
+          if (!"usuario" in ms || !"destino" in ms || !"mensaje" in ms){return;}
+            CrearMensaje(ms.usuario,ms.destino,ms.mensaje);
+        })
+        ServerEvents.addEventListener('open', function(s){
+            console.log("Conectao");
+        })
+        ServerEvents.addEventListener('error', function(s){
+            console.log("error")
+            setTimeout(() => connect(uri), (() => 1 * 1000)());
+        })
+    }
+
+}
+
+function init(){
+    ConectarseServer();
+
+    Mensajeador.addEventListener('submit', function(a){
+        a.preventDefault();
+        if (!Mensajeador.getElementById('mensaje').value){
+            return;
+        }
+        let Nombre = localStorage['nombre'];
+        let Destino = localStorage['destino'];
+        let Mensaje = Mensajeador.getElementById('mensaje').value;
+        fetch("/mensaje", {
+            method: "POST",
+            body: new URLSearchParams({Nombre,Destino,Mensaje}),
+        }).then(function(r) {
+                if (r.ok) Mensajeador.getElementById('mensaje').value = "";
+
+            })
+    })}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
+use rocket::form::{Form, FromForm};
 use rocket::fs::{relative, FileServer};
-use rocket::{fs::NamedFile, serde::{Deserialize, Serialize}, tokio::sync::broadcast::channel};
+use rocket::response::stream::{EventStream, Event};
+use rocket::tokio::select;
+use rocket::tokio::sync::broadcast::{channel, Sender, error::RecvError};
+use rocket::{fs::NamedFile, serde::{Deserialize, Serialize}, State, Shutdown};
 
 #[macro_use] extern crate rocket;
 
@@ -21,10 +25,37 @@ struct Sesion{
     destino: String,
     mensaje: String
 }
+
+#[post("/mensaje", data = "<ms>")]
+fn msg(ms: Form<Sesion>,q: &State<Sender<Sesion>>){
+    let _f = q.send(ms.into_inner());
+}
+
+#[get("/server")]
+async fn server(q: &State<Sender<Sesion>>, mut t: Shutdown) -> EventStream![]{
+    let mut Subs = q.subscribe();
+    EventStream! {
+        loop {
+            let ServM = select! {
+                ServM = Subs.recv() => match ServM {
+                    Ok(ServM) => ServM,
+                    Err(RecvError::Closed) => break,
+                    Err(RecvError::Lagged(_)) => continue,
+                },
+                _ = &mut t => break,
+            };
+            
+            yield Event::json(&ServM);
+        }
+    }
+
+
+}
+
 #[launch]
 fn rocket() -> _{
     rocket::build()
     .manage(channel::<Sesion>(500).0)
-    .mount("/", routes![index,lobby])
+    .mount("/", routes![index,lobby, msg, server])
     .mount("/", FileServer::from(relative!("files")))
 }
