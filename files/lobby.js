6e4cf6d73a7a6eb8482bc6fc806fe1ce8ed315b1
Gabriel Solotorevsky <gabysolotorevsky100@gmail.com>
2024-02-17 19:42:50 -0300

    Nuevo, con un error q voy a solucionar mañana

--- files/lobby.js
+++ files/lobby.js
@@ -20,17 +20,20 @@ FormLobby.hidden = true;
 let BotonAñadirLobby = document.getElementById("añadir");
 let NombreLobby = document.getElementById("nombre_lobby");
 let LobbysDiv = document.getElementById("lobbys");
+LobbysDiv.replaceChildren();
 
-let Lobbys = ["Main"];
+let Lobbys = [];
 
 if (localStorage["lobbys"]){
   console.log(JSON.parse(localStorage["lobbys"]));
   console.log(Lobbys);
-  Lobbys = JSON.parse(localStorage["lobbys"]);
-  for (let i = 0; i < Lobbys.length; i++){
-    CrearLobby(Lobbys[i]);
+  for (let i = 0; i < JSON.parse(localStorage["lobbys"]).length; i++){
+    console.log("Creando lobby con LocalStorage Lobbys ",JSON.parse(localStorage["lobbys"])[i]);
+    CrearLobby(JSON.parse(localStorage["lobbys"])[i]);
   }
+  Lobbys = JSON.parse(localStorage["lobbys"]);
 }
+
 
 //Si ya tiene guardado localmente un destino, acceder directamente al menu del chat
 if (localStorage["destino"]) {
@@ -40,10 +43,10 @@ if (localStorage["destino"]) {
   ConectarseServer();
   RecuperarMensajes();
   if (Lobbys.indexOf(localStorage["destino"]) == -1){
-    Lobbys.push(localStorage["destino"]);
-    localStorage["lobbys"] = JSON.stringify(Lobbys);
+    ActualizarLobbys(localStorage["destino"]);
   }
 }
+
 
 
 
@@ -55,9 +58,10 @@ BotonAñadirLobby.addEventListener("click", function(a){
 FormLobby.addEventListener("submit", function(a){
   a.preventDefault();
   if (!NombreLobby){FormLobby.hidden = true; return}
+  console.log("Creando lobby con el form ", NombreLobby.value);
   CrearLobby(NombreLobby.value);
-  Lobbys.push(NombreLobby.value);
-  localStorage["lobbys"] = JSON.stringify(Lobbys);
+  ActualizarLobbys(NombreLobby.value)
+  NombreLobby.value = "";
   FormLobby.hidden = true; 
 })
 
@@ -86,10 +90,9 @@ ConsolaForm.addEventListener("click", function (a) {
   Nombre.innerHTML = "Lobby " + localStorage["destino"];
   ConectarseServer();
   RecuperarMensajes();
-  Lobbys.push(localStorage["destino"]);
-  localStorage["lobbys"] = JSON.stringify(Lobbys);
-  CrearLobby("Main");
+  console.log("Creando lobby con el consola form ",localStorage["destino"])
   CrearLobby(localStorage["destino"]);
+  ActualizarLobbys(localStorage["destino"]);
   return true;
 });
 
@@ -114,36 +117,57 @@ Mensajeador.addEventListener("click", function (a) {
   return true;
 });
 
+function ActualizarLobbys(nombre){
+  if (Lobbys.indexOf(nombre) == -1 && !nombre == ""){
+    console.log("Antes: ",Lobbys);
+    Lobbys.push(nombre);
+    console.log("Despues: ",Lobbys);
+    localStorage["lobbys"] = JSON.stringify(Lobbys);
+    console.log("Local: ",JSON.parse(localStorage["lobbys"]));
+  }
+}
+
 function CrearLobby(nombre){
-  let a = document.createElement("button");
+  if (Lobbys.indexOf(nombre) == -1 && !nombre == ""){
+    let a = document.createElement("button");
   a.appendChild(document.createTextNode(nombre));
   let b = document.createElement("button");
   b.appendChild(document.createTextNode("-"));
-
-  a.appendChild(b);
-  LobbysDiv.appendChild(a);
+  let div = document.createElement("div");
+  div.appendChild(a);
+  div.appendChild(b);
+  LobbysDiv.appendChild(div);
 
   a.addEventListener("click", function(a){
     a.preventDefault();
     localStorage["destino"] = nombre;
     RecuperarMensajes();
+    Nombre.innerHTML = "Lobby " + localStorage["destino"];
   })
 
   b.addEventListener("click", function(d){
     d.preventDefault();
-    Lobbys.pop(nombre);
+    console.log("Antes: ",Lobbys);
+    Lobbys.splice(Lobbys.indexOf(nombre),1);
+    console.log("Despues: ",Lobbys);
     localStorage["lobbys"] = JSON.stringify(Lobbys);
+    console.log("Local: ",JSON.parse(localStorage["lobbys"]));
     console.log(Lobbys.length);
     if (Lobbys.length == 0){
       Consola.hidden = false;
       Chat.hidden = true;
       Nombre.innerHTML = "Lobby";
+      localStorage.removeItem("destino");
     }
     else{
       localStorage["destino"] = Lobbys[Lobbys.length - 1];
+      console.log("El destino es ", localStorage["destino"]);
+      Nombre.innerHTML = "Lobby " + localStorage["destino"];
+      RecuperarMensajes();
     }
-    LobbysDiv.removeChild(a);
+    LobbysDiv.removeChild(div);
   })
+  }
 }
 
 //Añade un mensaje en el chat del usuario
@@ -158,7 +182,6 @@ function CrearMensaje(usuario, destino, mensaje) {
 
 //TODO: Diferenciacion entre mensaje propio y de otro
 //TODO: Mensaje de entrar/salir de lobby
-//TODO: Multiples lobbys
 
 async function RecuperarMensajes(){
   Msg.replaceChildren();
@@ -185,34 +208,26 @@ async function RecuperarMensajes(){
 }
 
 async function ConectarseServer() {
-  let usuario = localStorage["nombre"];
-  let destino = localStorage["destino"];
-  let mensaje = "nashe";
+  const ServerEvents = new EventSource("/server");
 
-  let server = await fetch("/server",
-  {method: "POST",
-  body: new URLSearchParams({ usuario, destino, mensaje, activo })
+  ServerEvents.addEventListener("message", (s) => {
+    s.preventDefault();
+    let ms = JSON.parse(s.data);
+    if (!"usuario" in ms || !"destino" in ms || !"mensaje" in ms) {
+      return;
+    }
+    CrearMensaje(ms.usuario, ms.destino, ms.mensaje);
+    return true;
   });
-
-  const r = server.body.pipeThrough(new TextDecoderStream()).getReader()
-  while (true) {
-    if(!server.ok){
-      console.log("no conectao {}",v);
-      setTimeout(() => ConectarseServer(), (() => 1 * 1000)());
-    }
-    else{
-      const {value, done} = await r.read();
-      if (done) break;
-      if (!value.includes("usuario") || !value.includes("destino") || !value.includes("mensaje") ) {
-        console.log("Errorsito, no esta formateado");
-      }
-      else{
-        console.log(value.toString());
-        let parse = JSON.parse(value.replace("data:",""));
-        console.log(parse);
-        CrearMensaje(parse.usuario,parse.destino,parse.mensaje);
-      }
-    }
-  }
+  ServerEvents.addEventListener("open", function (s) {
+    console.log("Conectao");
+    return true;
+  });
+  ServerEvents.addEventListener("error", function (s) {
+    console.log("error " + s.data);
+    setTimeout(() => ConectarseServer(), (() => 1 * 1000)());
+    return false;
+  });
+  
   return true
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use core::num;
 use std::env;
 
 use rocket::form::{Form, FromForm};
@@ -9,7 +10,9 @@ use rocket::tokio::select;
 use rocket::tokio::sync::broadcast::{channel, Sender, error::RecvError};
 use rocket::{fs::NamedFile, serde::{Deserialize, Serialize}, State, Shutdown};
 use surrealdb::engine::local::File;
+use surrealdb::sql::{thing, Thing};
 use surrealdb::Surreal;
+use surrealdb::sql::Uuid;
 
 use surrealdb::engine::local::Db;
 #[macro_use] extern crate rocket;
@@ -33,31 +36,28 @@ struct Sesion{
     mensaje: String,
 }
 
+#[derive(Debug, Deserialize)]
+#[serde(crate = "rocket::serde")]
+struct Record {
+    #[allow(dead_code)]
+    id: Thing,
+}
+
 #[post("/mensaje", data = "<ms>")]
-async fn msg(ms: Form<Sesion>,q: &State<Sender<Sesion>>, db: &State<Surreal<Db>>){
+async fn msg(ms: Form<Sesion>,q: &State<Sender<Sesion>>, db: &State<Surreal<Db>>,numero_mensaje: &State<i32>){
     let c: Vec<Sesion> = db.create("mensajes")
     .content(Sesion {
         usuario: ms.usuario.to_string(),
         destino: ms.destino.to_string(),
         mensaje: ms.mensaje.to_string(),
     }).await.expect("Error en crear a mesias");
-    dbg!(c);
+    let s: Vec<Sesion> = db.select("mensajes").await.expect("msg");
+    for v in s {
+        println!("Mensaje: {}", v.mensaje);
+    }
     let _f = q.send(ms.into_inner());
 }
 
-/*async fn crear_msg(){
-    let c: Vec<Sesion> = db.create("mensajes")
-    .content(Sesion {
-        usuario: sesion.usuario.to_string(),
-        destino: sesion.destino.to_string(),
-        mensaje: sesion.mensaje.to_string(),
-    }).await.expect("Error en crear a mesias");
-    dbg!(c);
-    let a: Vec<Sesion> = db.select("usuarios").await.expect("Error en obtner usuarios");
-    let b: &String  = &a.get(0).unwrap().usuario;
-    println!("{a:?}");
-    println!("{b}");
-} */
 
 #[post["/restaurar",data = "<sesion>"]]
 async fn restaurar_mensajes(sesion: Form<Sesion>, db: &State<Surreal<Db>>) -> Json<Vec<Sesion>>{
@@ -67,17 +67,17 @@ async fn restaurar_mensajes(sesion: Form<Sesion>, db: &State<Surreal<Db>>) -> Js
         for s in a{
             if s.destino == sesion.destino {
                 envio.push(s);
+                
             }
         }
     }
+    dbg!(&envio);
     Json(envio)
 }
-
-//TODO: Mensajes de lobbys con SQL
+//TODO: Arreglar error: los mensajes no estan en orden
 //TODO: Lobbys viejos eliminan sus mensajes
-//TODO: Recuperacion de mensajes de lobbys
-#[post("/server", data = "<sesion>")]
-async fn server(sesion: Form<Sesion>,q: &State<Sender<Sesion>>, mut t: Shutdown) -> EventStream![]{
+#[get("/server")]
+async fn server(q: &State<Sender<Sesion>>, mut t: Shutdown) -> EventStream![]{
     let mut subs = q.subscribe();
     EventStream! {
         loop {
@@ -85,14 +85,7 @@ async fn server(sesion: Form<Sesion>,q: &State<Sender<Sesion>>, mut t: Shutdown)
                 serv_m = subs.recv() => match serv_m {
                     Ok(serv_m) => {
                         println!("Todo bein");
-                        if sesion.destino == serv_m.destino{
-                            println!("Es tu mensaje");
-                            serv_m
-                        }
-                        else {
-                            println!("no es tu mensaje");
-                            continue
-                        }
+                        serv_m
                         },
                     Err(RecvError::Closed) => {
                         println!("Error, se corto la conexion");
@@ -106,7 +99,7 @@ async fn server(sesion: Form<Sesion>,q: &State<Sender<Sesion>>, mut t: Shutdown)
 
             yield Event::json(&serv_m);
         }
-        println!("Ya no anda activo {}",sesion.usuario);
+        println!("Ya no anda activo");
     }
 
 
@@ -114,18 +107,10 @@ async fn server(sesion: Form<Sesion>,q: &State<Sender<Sesion>>, mut t: Shutdown)
 
 
 
-
 #[launch]
 async fn rocket() -> _ {
     let db: Surreal<Db> = Surreal::new::<File>("database").await.expect("No se cargo la base de datos en rocket launch");
     db.use_ns("Chaos").use_db("JJW").await.expect("Error en cambiar de ns y db");
-    let c: Vec<Sesion> = db.create("mensajes")
-    .content(Sesion {
-        usuario: "Miguelito".to_string(),
-        destino: "Main".to_string(),
-        mensaje: "Prueba".to_string(),
-    }).await.expect("Error en crear a mesias");
-    dbg!(c);
     rocket::build()
     .manage(channel::<Sesion>(500).0)
     .manage(db)
