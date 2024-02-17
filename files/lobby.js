35b03febe87dc5b754d71e345f9fe1ace544510d
Gabriel Solotorevsky <gabysolotorevsky100@gmail.com>
2024-02-17 17:16:12 -0300

    Menu de lobbys

--- files/lobby.js
+++ files/lobby.js
@@ -15,6 +15,23 @@ let Nombre = document.getElementById("nombreChat");
 let Msg = document.getElementById("mensajes");
 let Mensajeador = document.getElementById("meth");
 
+let FormLobby = document.getElementById("form_lobby");
+FormLobby.hidden = true;
+let BotonAñadirLobby = document.getElementById("añadir");
+let NombreLobby = document.getElementById("nombre_lobby");
+let LobbysDiv = document.getElementById("lobbys");
+
+let Lobbys = ["Main"];
+
+if (localStorage["lobbys"]){
+  console.log(JSON.parse(localStorage["lobbys"]));
+  console.log(Lobbys);
+  Lobbys = JSON.parse(localStorage["lobbys"]);
+  for (let i = 0; i < Lobbys.length; i++){
+    CrearLobby(Lobbys[i]);
+  }
+}
+
 //Si ya tiene guardado localmente un destino, acceder directamente al menu del chat
 if (localStorage["destino"]) {
   Consola.hidden = true;
@@ -22,7 +39,27 @@ if (localStorage["destino"]) {
   Nombre.innerHTML = "Lobby " + localStorage["destino"];
   ConectarseServer();
   RecuperarMensajes();
+  if (Lobbys.indexOf(localStorage["destino"]) == -1){
+    Lobbys.push(localStorage["destino"]);
+    localStorage["lobbys"] = JSON.stringify(Lobbys);
+  }
 }
+
+
+
+BotonAñadirLobby.addEventListener("click", function(a){
+  a.preventDefault();
+  FormLobby.hidden = false;
+})
+
+FormLobby.addEventListener("submit", function(a){
+  a.preventDefault();
+  if (!NombreLobby){FormLobby.hidden = true; return}
+  CrearLobby(NombreLobby.value);
+  Lobbys.push(NombreLobby.value);
+  localStorage["lobbys"] = JSON.stringify(Lobbys);
+  FormLobby.hidden = true; 
+})
 
 //Boton para volver al menu de sesion
 Volver.addEventListener("click", function () {
@@ -49,6 +86,10 @@ ConsolaForm.addEventListener("click", function (a) {
   Nombre.innerHTML = "Lobby " + localStorage["destino"];
   ConectarseServer();
   RecuperarMensajes();
+  Lobbys.push(localStorage["destino"]);
+  localStorage["lobbys"] = JSON.stringify(Lobbys);
+  CrearLobby("Main");
+  CrearLobby(localStorage["destino"]);
   return true;
 });
 
@@ -72,6 +113,38 @@ Mensajeador.addEventListener("click", function (a) {
   });
   return true;
 });
+
+function CrearLobby(nombre){
+  let a = document.createElement("button");
+  a.appendChild(document.createTextNode(nombre));
+  let b = document.createElement("button");
+  b.appendChild(document.createTextNode("-"));
+
+  a.appendChild(b);
+  LobbysDiv.appendChild(a);
+
+  a.addEventListener("click", function(a){
+    a.preventDefault();
+    localStorage["destino"] = nombre;
+    RecuperarMensajes();
+  })
+
+  b.addEventListener("click", function(d){
+    d.preventDefault();
+    Lobbys.pop(nombre);
+    localStorage["lobbys"] = JSON.stringify(Lobbys);
+    console.log(Lobbys.length);
+    if (Lobbys.length == 0){
+      Consola.hidden = false;
+      Chat.hidden = true;
+      Nombre.innerHTML = "Lobby";
+    }
+    else{
+      localStorage["destino"] = Lobbys[Lobbys.length - 1];
+    }
+    LobbysDiv.removeChild(a);
+  })
+}
 
 //Añade un mensaje en el chat del usuario
 function CrearMensaje(usuario, destino, mensaje) {
